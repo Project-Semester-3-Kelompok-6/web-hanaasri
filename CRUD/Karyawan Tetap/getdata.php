294b71b64d6e441fcbc85b07c3649b1d34fbfc26
DvdArdiansyahhh <124945304+DvdArdiansyahhh@users.noreply.github.com>
2023-11-02 11:02:18 +0700

    DataTable Karyawa Tetap

---
+++ CRUD/Karyawan Tetap/getdata.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="sidebar.css">
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+</head>
+
+<body>
+    <div class="sidebar">
+
+    </div>
+    <table id="example" class="display" style="width:100%">
+        <thead>
+            <tr>
+                <th data-orderable="false">No</th>
+                <th>Nama Karyawan</th>
+                <th>Devisi</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            include "database.php";
+            $karyawan_tp = mysqli_query($conn, "SELECT users.Nama AS NamaKaryawan, devisi.NamaDevisi AS NamaDevisi
+                                            FROM users
+                                            INNER JOIN devisi ON users.DevisiID = devisi.DevisiID
+                                            WHERE users.Status = 'Karyawan tetap'");
+            $nomor = 1;
+            while ($row = mysqli_fetch_array($karyawan_tp)) {
+                echo "<tr>
+            <td>$nomor</td>
+            <td>" . $row['NamaKaryawan'] . "</td>
+            <td>" . $row['NamaDevisi'] . "</td>
+            <td></td>
+            </tr>";
+                $nomor++; //Tambah nomor urut(otomatis)
+            }
+            ?>
+        </tbody>
+    </table>
+    <script>
+        $(document).ready(function() {
+            var table = $('#example').DataTable({
+                "order": []
+            });
+
+            // Menangani reset nomor urut saat data disorting
+            table.on('order.dt search.dt', function() {
+                table.column(0, {
+                    search: 'applied',
+                    order: 'applied'
+                }).nodes().each(function(cell, i) {
+                    cell.innerHTML = i + 1;
+                });
+            }).draw();
+        });
+    </script>
+</body>
+
+</html>
--- login.php
+++ login.php
@@ -23,7 +23,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
         exit();
     }
 
-    $sql = "SELECT * FROM account WHERE email ='$email' AND password = '$password'";
+    $sql = "SELECT * FROM users WHERE email ='$email' AND password = '$password'";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) == 1) {
