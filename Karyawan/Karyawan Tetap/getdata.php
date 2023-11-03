748143603b928a63eab5beb63466c1d35bbe5607
DvdArdiansyahhh <124945304+DvdArdiansyahhh@users.noreply.github.com>
2023-11-03 16:15:27 +0700

    Karyawan Tetap

--- Karyawan/Karyawan Tetap/getdata.php
+++ Karyawan/Karyawan Tetap/getdata.php
@@ -10,8 +10,8 @@
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 </head>
-
 <body>
+    <section>
     <div class="sidebar">
 
     </div>
@@ -61,6 +61,7 @@
             }).draw();
         });
     </script>
+    </section>
 </body>
 
 </html>
