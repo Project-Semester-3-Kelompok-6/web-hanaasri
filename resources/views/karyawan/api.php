81904ade6aa9db86018f28535b0a21e91e2483f6
DvdArdiansyahhh <124945304+DvdArdiansyahhh@users.noreply.github.com>
2023-11-23 13:36:15 +0700

    Password on detail, Modal Tambahdata

--- resources/views/karyawan/api.php
+++ resources/views/karyawan/api.php
@@ -16,14 +16,14 @@ if (isset($_GET['action'])) {
             if ($action == 'get_users') {
                 if (isset($_GET['status'])) {
                     $status = $_GET['status'];
-            
+
                     $query = "SELECT users.*, devisi.NamaDevisi
                               FROM users
                               INNER JOIN devisi ON users.DevisiID = devisi.DevisiID
                               WHERE users.Status = $status";
-            
+
                     $result = $conn->query($query);
-            
+
                     if ($result) {
                         $users = $result->fetch_all(MYSQLI_ASSOC);
                         echo json_encode($users);
@@ -37,15 +37,60 @@ if (isset($_GET['action'])) {
                 }
             }
             break;
+            // Tambahkan endpoint untuk mendapatkan data devisi
+            if ($action == 'get_devisi') {
+                $query = "SELECT * FROM devisi";
+                $result = $conn->query($query);
+            
+                if ($result) {
+                    $devisi = $result->fetch_all(MYSQLI_ASSOC);
+                    echo json_encode($devisi);
+                } else {
+                    http_response_code(500);
+                    echo json_encode(array("message" => "Error retrieving devisi data"));
+                }
+            }
+            break;
+
+            if ($action == 'get_user_detail') {
+                if (isset($_GET['id'])) {
+                    $userId = $_GET['id'];
+
+                    $query = "SELECT users.*, devisi.NamaDevisi
+                      FROM users
+                      INNER JOIN devisi ON users.DevisiID = devisi.DevisiID
+                      WHERE users.UserID = $userId";
+
+                    $result = $conn->query($query);
+
+                    if ($result) {
+                        $userDetail = $result->fetch_assoc();
+                        echo json_encode($userDetail);
+                    } else {
+                        http_response_code(500);
+                        echo json_encode(array("message" => "Error retrieving user details"));
+                    }
+                } else {
+                    http_response_code(400);
+                    echo json_encode(array("message" => "'id' parameter is missing"));
+                }
+            }
+            break;
+
 
         case 'POST':  //belum
-            // Menambah data
-            // Contoh: api.php?action=add_user&name=John&email=john@example.com
+            // Tambahkan endpoint untuk menambahkan data user
             if ($action == 'add_user') {
-                $name = $_POST['name'];
+                $nama = $_POST['nama'];
                 $email = $_POST['email'];
+                $password = $_POST['password'];
+                $divisiID = $_POST['divisiID'];
+                $role = 'Karyawan';
+                $status = 'Karyawan Tetap';
 
-                $query = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+                $query = "INSERT INTO users (Nama, Email, Password, DevisiID, Role, Status)
+              VALUES ('$nama', '$email', '$password', '$divisiID', '$role', '$status')";
+
                 if ($conn->query($query)) {
                     echo json_encode(array("message" => "User added successfully"));
                 } else {
@@ -54,28 +99,28 @@ if (isset($_GET['action'])) {
                 }
             }
             break;
-            
-            case 'DELETE':
-                // Menghapus data
-                // Contoh: api.php?action=delete_user&id=1
-                if ($action == 'delete_user') {
-                    if (isset($_GET['id'])) {
-                        $id = $_GET['id'];
-            
-                        $query = "DELETE FROM users WHERE UserID=$id";
-                        if ($conn->query($query)) {
-                            echo json_encode(array("message" => "User deleted successfully"));
-                        } else {
-                            http_response_code(500);
-                            echo json_encode(array("message" => "Error deleting user"));
-                        }
+
+        case 'DELETE':
+            // Menghapus data
+            // Contoh: api.php?action=delete_user&id=1
+            if ($action == 'delete_user') {
+                if (isset($_GET['id'])) {
+                    $id = $_GET['id'];
+
+                    $query = "DELETE FROM users WHERE UserID=$id";
+                    if ($conn->query($query)) {
+                        echo json_encode(array("message" => "User deleted successfully"));
                     } else {
-                        http_response_code(400);
-                        echo json_encode(array("message" => "'id' parameter is missing"));
+                        http_response_code(500);
+                        echo json_encode(array("message" => "Error deleting user"));
                     }
+                } else {
+                    http_response_code(400);
+                    echo json_encode(array("message" => "'id' parameter is missing"));
                 }
-                break;
-            
+            }
+            break;
+
         default:
             // Metode HTTP tidak didukung
             http_response_code(405);
@@ -89,4 +134,3 @@ if (isset($_GET['action'])) {
 }
 
 $conn->close();
-?>
